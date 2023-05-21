192e6c9e79ad5cc2dd65e828300283154894c4c8
agustino42 <lopezagustin26@gmail.com>
2023-05-20 21:28:47 -0400

    modelado caja-carrito (Primeros Modelos)

---
+++ assets/js/caja.js
@@ -0,0 +1,186 @@
+// PRODUCTOS
+const productos = [
+    // Hortalizas
+    {
+        id: "zanahoria-01",
+        titulo: "Zanahoria 01",
+        imagen: "",
+        categoria: {
+            nombre: "Hortalizas",
+            id: "hortalizas"
+        },
+        precio: 1000
+    },
+    {
+        id: "limon-02",
+        titulo: "Limon 02",
+        imagen: "",
+        categoria: {
+            nombre: "Hortalizas",
+            id: "hortalizas"
+        },
+        precio: 1000
+    },
+    {
+        id: "ajo-03",
+        titulo: "Ajo",
+        imagen: "./img/abrigos/03.jpg",
+        categoria: {
+            nombre: "Hortalizas",
+            id: "hortalizas"
+        },
+        precio: 1000
+    },
+    {
+        id: "cebolla-04",
+        titulo: "Cebolla 04",
+        imagen: "./img/abrigos/04.jpg",
+        categoria: {
+            nombre: "Hortalizas",
+            id: "hortalizas"
+        },
+        precio: 1000
+    },
+    {
+        id: "papas-05",
+        titulo: "Papas 05",
+        imagen: "",
+        categoria: {
+            nombre: "Hortizas",
+            id: "hortalizas"
+        },
+        precio: 1000
+    },
+    // Frigorifico
+    {
+        id: "muslo-01",
+        titulo: "Muslo 01",
+        imagen: "",
+        categoria: {
+            nombre: "frigorifico",
+            id: "frigorifico"
+        },
+        precio: 1000
+    },
+    {
+        id: "pollo-02",
+        titulo: "Pollo 02",
+        imagen: "",
+        categoria: {
+            nombre: "Frigorifico",
+            id: "frigorifico"
+        },
+        precio: 1000
+    },
+    {
+        id: "Hueso-03",
+        titulo: "Hueso 03",
+        imagen: "",
+        categoria: {
+            nombre: "Frigorifico",
+            id: "frigorifico"
+        },
+        precio: 1000
+    },
+    {
+        id: "punta-04",
+        titulo: "Punta 04",
+        imagen: "",
+        categoria: {
+            nombre: "Frigorifico",
+            id: "frigorifico"
+        },
+        precio: 1000
+    },
+    {
+        id: "solomo-05",
+        titulo: "Solomo 05",
+        imagen: "",
+        categoria: {
+            nombre: "Frigorifico",
+            id: "frigorifico"
+        },
+        precio: 1000
+    },
+    {
+        id: "lomito-06",
+        titulo: "Lomito 06",
+        imagen: "",
+        categoria: {
+            nombre: "Frigorifico",
+            id: "frigorifico"
+        },
+        precio: 1000
+    },
+    {
+        id: "costilla-07",
+        titulo: "Costilla 07",
+        imagen: "",
+        categoria: {
+            nombre: "Frigorifico",
+            id: "frigorifico"
+        },
+        precio: 1000
+    },
+    {
+        id: "carne-08",
+        titulo: "Carnes 08",
+        imagen: "",
+        categoria: {
+            nombre: "Frigorifico",
+            id: "frigorifico"
+        },
+        precio: 1000
+    },
+    // Mercado
+    {
+        id: "sardinas-01",
+        titulo: "Sardinas 01",
+        imagen: "",
+        categoria: {
+            nombre: "Mercado",
+            id: "mercado"
+        },
+        precio: 1000
+    },
+    {
+        id: "Atum-02",
+        titulo: "Atum 02",
+        imagen: "",
+        categoria: {
+            nombre: "Mercado",
+            id: "mercado"
+        },
+        precio: 1000
+    },
+    {
+        id: "arroz-03",
+        titulo: "Arroz 03",
+        imagen: "",
+        categoria: {
+            nombre: "Mercado",
+            id: "mercado"
+        },
+        precio: 1000
+    },
+    {
+        id: "pasta-04",
+        titulo: "Pasta 04",
+        imagen: "",
+        categoria: {
+            nombre: "Mercado",
+            id: "mercado"
+        },
+        precio: 1000
+    },
+    {
+        id: "harina-05",
+        titulo: "Harina 05",
+        imagen: "",
+        categoria: {
+            nombre: "Mercado",
+            id: "mercado"
+        },
+        precio: 1000
+    }
+];
